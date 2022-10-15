ab976cbd1ef2cd209401476c24c4e423c4e3f57b
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2022-10-15 06:45:42 -0700

    Print a string followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - program entry
+ * Return: always return 0 (success)
+ */
+
+	int main()
+{
+ 	printf("Programming is like building a multilingual puzzle"\n);
+	puts("Programming is like building a multilingual puzzle" *s)
+}
