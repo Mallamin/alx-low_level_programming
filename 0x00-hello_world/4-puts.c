0c670b851333332eb60abc24aacb2b32d3901fdc
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2022-10-15 06:50:29 -0700

    Print a string followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
 
 	int main()
 {
- 	printf("Programming is like building a multilingual puzzle"\n);
-	puts("Programming is like building a multilingual puzzle" *s)
+ 	printf("Programming is like building a multilingual puzzle")\n;
+	puts("Programming is like building a multilingual puzzle" *s);
 }
