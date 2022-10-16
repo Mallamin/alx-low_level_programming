bc8770b46a1c0ec439ac665f30bcd48bd4d38443
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2022-10-16 13:44:21 -0700

    Print the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
  * main - entry point
  * Return: always 0 (success)
  */
-int main(void)
-{
+/*int main(void)*/
+/*{*/
 	/**
 	 * int s;
 	*float f;
@@ -13,10 +13,19 @@ int main(void)
 	*long long int j;
 	*/
 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
-	printf("Size of a char : %c byte(s)\n", (int)sizeof(char));
-	printf("Size of int r: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of int j: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-	return (0);
+	/**
+	 * printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	*printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	*printf("Size of a char : %c byte(s)\n", (int)sizeof(char));
+	*printf("Size of int r: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	*printf("Size of int j: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	*return (0);
+	*/
+int main()
+{
+	printf("size of int : %d\n",sizeof(int));
+	printf("size of signed int : %d\n",sizeof(signed int));
+	printf("size of unsigned long : %d\n",sizeof(unsigned long));
+	return 0;
 }
+/*}*/
